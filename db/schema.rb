92d4b9d95727271ef99a2ebcd34a7cf5abaa829d
Indira Vanhulle <indira_vanhulle@hotmail.com>
2021-08-23 18:11:30 +0200

    updated foreign keys ids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_154434) do
+ActiveRecord::Schema.define(version: 2021_08_23_161020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,10 @@ ActiveRecord::Schema.define(version: 2021_08_23_154434) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
   add_foreign_key "bookings", "boardgames"
+  add_foreign_key "reviews", "bookings"
 end
