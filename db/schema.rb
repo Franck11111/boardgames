2b3bdd5ff77683e7b27b9a7d3331b94608a35f45
Franck Dirat <franck.dirat@gmail.com>
2021-08-24 13:20:03 +0200

    pundit almost done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_091715) do
+ActiveRecord::Schema.define(version: 2021_08_24_110454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_08_24_091715) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_per_day"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_boardgames_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -55,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_091715) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boardgames", "users"
   add_foreign_key "bookings", "boardgames"
   add_foreign_key "reviews", "bookings"
 end
