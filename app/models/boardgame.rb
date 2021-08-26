f5e9e37532cece0682240241f9277fc891f19f3e
Freeelu <freeelu@gmail.com>
2021-08-26 17:20:35 +0200

    cloudinary set up

--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,7 +1,7 @@
 class Boardgame < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
-  has_many_attached :photos
+  has_one_attached :photo
   belongs_to :user
 
   validates :title, :description, :price_per_day, presence: true
